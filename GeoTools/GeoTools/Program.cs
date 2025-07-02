c428424aa41c453ed43df3f7a7e9e90b734fc0eb
Hinnerk Weiler <post@hinnerk-weiler.de>
2025-07-02 07:31:25 +0200

    adding Controllers

--- GeoTools/GeoTools/Program.cs
+++ GeoTools/GeoTools/Program.cs
@@ -78,5 +78,6 @@ app.MapRazorComponents<App>()
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
+app.MapControllers();
 
 app.Run();
